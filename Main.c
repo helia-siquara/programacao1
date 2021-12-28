d1f12b7a208c2cce4179ae356e91002a97abefe0
gabrielol113 <gabrielol.113@hotmail.com>
2021-12-28 21:45:51 +0000

    Código adicionado switches

--- Main.c
+++ Main.c
@@ -4,21 +4,58 @@
 
 void exibeMenu(){
 	printf("\t\t\tMenu\n");
-	printf("\t[1] Inserir informa��o das Moedas\n");
+	printf("\t[1] Inserir informação das Moedas\n");
 	printf("\t[2] Listar todos os Registos de Moedas inseridos\n");
-	printf("\t[3] Listar Moedas por Estado de Conserva��o\n");
-	printf("\t[4] Remover Moedas por N�mero de Registo\n");
+	printf("\t[3] Listar Moedas por Estado de Conservação\n");
+	printf("\t[4] Remover Moedas por Número de Registo\n");
 	printf("\t[5] Gravar Registos de Moedas no Ficheiro\n");
 	printf("\t[6] Ler registos do Ficheiro\n");
 	printf("\t[0] Sair;\n");
-	printf("\nQual fun��o deseja executar:");
+	printf("\nQual função deseja executar:");
 }
 
 void InsereMoeda(){
 	
 }
+// começando já a fazer as funções para dps separar tudo mas com o tempo a gente faz isso
+void main(){
 
-main(){
+	int op;
 	setlocale(LC_ALL, "Portuguese");
-	exibeMenu();
+
+
+// Eu não sei se é melhor pra usar switch ou se vários ifs fica mais visualmente bonito
+	while(1){
+		exibeMenu();
+		scanf("%d", &op);
+		switch (op)
+		{
+		case 0:
+			break;
+		case 1:
+			InsereMoeda();
+			break;
+		case 2:
+			/* code */
+			break;
+		case 3:
+			/* code */
+			break;
+		case 4:
+			/* code */
+			break;
+		case 5:
+			/* code */
+			break;
+		case 6:
+			/* code */
+			break;
+		case 7:
+			/* code */
+			break;
+		default:
+			printf("Selção inválida");
+			break;
+		}
+	}
 }
--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # programacao1
 Trabalho usado para obter nota para passar na matéria de Programação Imperativa
+
+
+Comecei preparando os cases vamos fazer as funções dps jogamos pra main eu vou deixar comentários e se precisar de lembrar de alguma coisa deixarei no readme os passos que temos que fazer
+
+
+
